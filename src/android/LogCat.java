056016e8c0e8dcda7b280e4d8d5d266d32ac6662
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-06-05 14:42:16 +0100

    Update LogCat.java

--- src/android/LogCat.java
+++ src/android/LogCat.java
@@ -26,25 +26,30 @@ public class LogCat extends CordovaPlugin { //LogCatPlugin
 	    super.pluginInitialize();
     }*/
 
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) 
-	      throws JSONException {
+    	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 	    if (action.equals("sendLogs")) {
-		     Log.i(TAG, "Sendlogs received");
-            if(!foregroundServiceRunning()) {
-		Log.i(TAG, "NO foreground service running");
-		Activity activity = cordova.getActivity();
-                Intent serviceIntent = new Intent(activity, MyForegroundService.class);
-                activity.getApplicationContext().startForegroundService(serviceIntent);
-		Log.i(TAG, "Foreground service LogCat Plugin running");
-            }  
-		    //create a new Intent to send the logs
-		    //Intent serviceIntent = new Intent(cordova.getActivity(), MyForegroundService.class);
-		    //serviceIntent.setaction(""); //string of the action that want to execute
-             return true;
-	    }else{        
-	    return false;
+		Log.i(TAG, "Sendlogs received");
+            	if(!foregroundServiceRunning()) {
+			
+			Log.i(TAG, "NO foreground service running");
+			
+			Activity activity = cordova.getActivity();
+                	Intent serviceIntent = new Intent(activity, MyForegroundService.class);
+                	activity.getApplicationContext().startForegroundService(serviceIntent);
+			
+			Log.i(TAG, "Foreground service LogCat Plugin running");
+            	}  
+		    
+		//create a new Intent to send the logs
+		    
+		//Intent serviceIntent = new Intent(cordova.getActivity(), MyForegroundService.class);
+		//serviceIntent.setaction(""); //string of the action that want to execute
+             	return true;
+		    
+	    } else {        
+	    	return false;
 	    }
-      }	
+      	}	
     
     /*@Override
     protected void onCreate(Bundle savedInstanceState) {
