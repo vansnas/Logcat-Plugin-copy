81a00fa44fbc64aa87495f02088483042af677be
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-06-06 14:30:31 +0100

    Update LogCat.java

--- src/android/LogCat.java
+++ src/android/LogCat.java
@@ -1,3 +1,4 @@
+package org.apache.cordova.logcat;
 
 import android.app.ActivityManager;
 import android.content.Context;
@@ -13,44 +14,43 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.os.Environment;
 import android.app.Activity;
-//import org.apache.cordova.logcat.MyForegroundService;
 import android.util.Log;
 
-public class LogCat extends CordovaPlugin { //LogCatPlugin 
-	
-	private static final String TAG = "LogCatPlugin";
+public class LogCat extends CordovaPlugin { //LogCatPlugin
+
+    private static final String TAG = "LogCatPlugin";
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-	    if (action.equals("sendLogs")) {
+        if (action.equals("sendLogs")) {
             if(!foregroundServiceRunning()) {
-			
-				Activity activity = cordova.getActivity();
+
+                Activity activity = cordova.getActivity();
                 Intent serviceIntent = new Intent(activity, MyForegroundService.class);
                 activity.getApplicationContext().startForegroundService(serviceIntent);
-			
-            }  
-		    
-		//create a new Intent to send the logs
-		    
-		//Intent serviceIntent = new Intent(cordova.getActivity(), MyForegroundService.class);
-		//serviceIntent.setaction(""); //string of the action that want to execute
-             return true;
-		    
-	    } else {        
-	    	return false;
-	    }
-		
-    }	
+
+            }
+
+            //create a new Intent to send the logs
+
+            //Intent serviceIntent = new Intent(cordova.getActivity(), MyForegroundService.class);
+            //serviceIntent.setaction(""); //string of the action that want to execute
+            return true;
+
+        } else {
+            return false;
+        }
+
+    }
 
     public boolean foregroundServiceRunning() {
-    	Context context = cordova.getActivity().getApplicationContext();
-    	ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-    	for (ActivityManager.RunningServiceInfo service : activityManager.getRunningServices(Integer.MAX_VALUE)) {
-        	if (MyForegroundService.class.getName().equals(service.service.getClassName())) {
-            		return true;
-        	}
-    	}
-    	return false;
+        Context context = cordova.getActivity().getApplicationContext();
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : activityManager.getRunningServices(Integer.MAX_VALUE)) {
+            if (MyForegroundService.class.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
