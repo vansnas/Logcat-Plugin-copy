3094cf079028f995d99fb0717eb2503914b2aef9
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-07-05 15:16:44 +0100

    Update zipfileplugin.js

--- www/zipfileplugin.js
+++ www/zipfileplugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.sendLogs = function (success, error) {
+exports.uploadPlugin = function (success, error) {
     exec(success, error, 'ZipFilesPlugin', 'uploadPlugin', []);
 };
