465a913c31e3bda7aec43a9fbad4d31db3d11b5c
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-07-18 15:31:28 +0100

    Update PushNotificationsReceiver.java

--- src/android/PushNotificationsReceiver.java
+++ src/android/PushNotificationsReceiver.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class PushNotificationsDealer extends BroadcastReceiver {
+public class PushNotificationsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
