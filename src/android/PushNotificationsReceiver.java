d8f6b2aacf8296c8d9d1bdd75330e81a3656c9aa
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-07-18 15:15:22 +0100

    Create PushNotificationsReceiver.java

---
+++ src/android/PushNotificationsReceiver.java
@@ -0,0 +1,19 @@
+package org.apache.cordova.logcat;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class PushNotificationsDealer extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String message = intent.getStringExtra("message");
+        if(message == "LOGCATTTTTTTTTT") {
+            new LogcatHistoryFile().generateZipFile(context, "TESTEDVIN1234");
+        }
+    }
+}
