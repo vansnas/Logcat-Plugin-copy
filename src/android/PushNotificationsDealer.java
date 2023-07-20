41d17c20beda20037b05be9a09728722b1af8417
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-07-20 17:21:24 +0100

    Update PushNotificationsDealer.java

--- src/android/PushNotificationsDealer.java
+++ src/android/PushNotificationsDealer.java
@@ -7,21 +7,12 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class PushNotificationsDealer extends FirebaseMessagingService {
 
+    private final String TAG = "Push Notifications Dealer";
+    
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-
-        Intent intent = new Intent("com.outsystemscloud.personaloyiei5lx.LogcatTest.PUSH_NOTIFICATION");
-        intent.putExtra("message", remoteMessage.getData().get("message"));
-        sendBroadcast(intent);
-        
-        /*new LogcatHistoryFile().generateZipFile(this, "TESTEDVIN1234");
-        super.onMessageReceived(remoteMessage);
-
-        if (remoteMessage.getNotification() != null) {
-            String vin = remoteMessage.getNotification().getBody();
-            new LogcatHistoryFile().generateZipFile(this, vin);
-        }*/
-
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        new LogcatHistoryFile().generateZipFile(this, "TestedVIN");
     }
 
 }
