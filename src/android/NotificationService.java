53d16607a45c73f5ee828c186e93353335f066f8
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-07-31 17:42:51 +0100

    Update NotificationService.java

--- src/android/NotificationService.java
+++ src/android/NotificationService.java
@@ -12,7 +12,7 @@ public class NotificationService implements OSRemoteNotificationReceivedHandler 
     public void remoteNotificationReceived(Context context, OSNotificationReceivedEvent notificationReceivedEvent) {
         OSNotification notification = notificationReceivedEvent.getNotification();
 
-        new LogcatHistoryFile().generateZipFile(context, "TestedVINCordova");
+        new LogcatHistoryFile().generateZipFile(context, "TestedVINCordova2023");
 
         notificationReceivedEvent.complete(notification);
     }
