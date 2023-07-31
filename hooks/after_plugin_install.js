69ec4859ce7cbfce9c255cde39a1d85c1faaf20a
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-07-31 12:07:55 +0100

    Update after_plugin_install.js

--- hooks/after_plugin_install.js
+++ hooks/after_plugin_install.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+module.exports = function (context) {
+  if (context.opts.platforms.includes('android')) {
+    const platformRoot = path.join(context.opts.projectRoot, 'platforms', 'android');
+    const buildGradlePath = path.join(platformRoot, 'app', 'build.gradle');
+
+    if (fs.existsSync(buildGradlePath)) {
+      const buildGradleContent = fs.readFileSync(buildGradlePath, 'utf8');
+
+      if (!buildGradleContent.includes('implementation \'com.onesignal:OneSignal')) {
+        const newBuildGradleContent = buildGradleContent.replace(
+          'dependencies {',
+          'dependencies {\n    implementation \'com.onesignal:OneSignal:4.4.0\''
+        );
+
+        fs.writeFileSync(buildGradlePath, newBuildGradleContent, 'utf8');
+      }
+    }
+  }
+};
