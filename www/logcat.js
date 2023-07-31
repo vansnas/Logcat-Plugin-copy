d99b101f10b13d237d09f8aea720a49d97783fc8
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-07-31 17:45:13 +0100

    Update logcat.js

--- www/logcat.js
+++ www/logcat.js
@@ -11,36 +11,3 @@ exports.uploadPlugin = function (vin, success, error) {
 exports.registerDevice = function (appid, success, error) {
     exec(success, error, 'LogCat', 'uploadPlugin', [appid]);
 };
-
-/*document.addEventListener('notificationReceived', function(event) {
-    var notification = event.data.notification;
-    
-    var title = notification.title;
-    var message = notification.body;
-    var customData = notification.additionalData;
-
-    exports.uploadPlugin(
-        'Test1111111VIN',
-        function(successData) {
-            console.log('Logs sent successfully');
-        },
-        function(errorData) {
-            console.log('Failed to send logs:', errorData);
-        }
-    );
-});
-
-window.plugins.OneSignal.handleNotificationOpened(function(jsonData) {
-    var notification = jsonData.notification;
-
-    exports.uploadPlugin(
-        'Test2222222VIN',
-        function(successData) {
-            console.log('Logs sent successfully');
-        },
-        function(errorData) {
-            console.log('Failed to send logs:', errorData);
-        }
-    );
-    
-});*/
