c20f24b52d965c997c9a6626e09f5e5886464edd
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-09-08 17:11:57 +0100

    Update logcat.js

--- www/logcat.js
+++ www/logcat.js
@@ -11,3 +11,11 @@ exports.uploadPlugin = function (vin, success, error) {
 exports.registerDevice = function (appid, success, error) {
     exec(success, error, 'LogCat', 'registerDevice', [appid]);
 };
+
+OneSignalPlugin.prototype.addSubscriptionObserver = function(callback) {
+    OneSignalPlugin._subscriptionObserverList.push(callback);
+    var subscriptionCallBackProcessor = function(state) {
+        OneSignalPlugin._processFunctionList(OneSignalPlugin._subscriptionObserverList, new OSSubscriptionStateChanges(state));
+    };
+    window.cordova.exec(subscriptionCallBackProcessor, function(){}, "OneSignalPush", "addSubscriptionObserver", []);
+};
