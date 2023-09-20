0b08dff86581ca6509401fc7b708b0a1d1b5b280
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-09-20 17:26:12 +0100

    Delete www/InAppMessage.js

--- www/InAppMessage.js
+++
@@ -1,8 +0,0 @@
-function OSInAppMessageAction (json) {
-    this.clickName = json.click_name;
-    this.clickUrl = json.click_url;
-    this.firstClick = json.first_click;
-    this.closesMessage = json.closes_message;
-}
-
-module.exports = OSInAppMessageAction;
