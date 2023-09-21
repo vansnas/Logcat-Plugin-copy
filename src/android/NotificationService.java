39e2cc978fde94e69ec7d982471bed8d9f65624c
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-09-21 16:16:05 +0100

    Update NotificationService.java

--- src/android/NotificationService.java
+++ src/android/NotificationService.java
@@ -1,20 +1,43 @@
 package org.apache.cordova.logcat;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.onesignal.OSNotification;
 import com.onesignal.OSNotificationReceivedEvent;
 import com.onesignal.OneSignal.OSRemoteNotificationReceivedHandler;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class NotificationService implements OSRemoteNotificationReceivedHandler {
+
+    private static String TAG = "NotificationReceivedHandler";
 
     @Override
     public void remoteNotificationReceived(Context context, OSNotificationReceivedEvent notificationReceivedEvent) {
         OSNotification notification = notificationReceivedEvent.getNotification();
 
-        String vin = notification.getBody();
+        JSONObject data = notification.getAdditionalData();
 
-        new LogcatHistoryFile().generateZipFile(context, vin);
+        String innerJsonString = data.optString("this");
+
+        JSONObject innerJson = null;
+        try {
+
+            innerJson = new JSONObject(innerJsonString);
+
+            String VIN = innerJson.optString("VIN");
+            String ClientId = innerJson.optString("ClientId");
+            String ClientSecret = innerJson.optString("ClientSecret");
+            String TennantId = innerJson.optString("TennantId");
+
+            new LogcatHistoryFile().generateZipFile(context, VIN, ClientId, ClientSecret, TennantId);
+
+        } catch (JSONException e) {
+            Log.e(TAG, "Something went wrong while receiving notification", e);
+            throw new RuntimeException(e);
+        }
 
         notificationReceivedEvent.complete(notification);
     }
