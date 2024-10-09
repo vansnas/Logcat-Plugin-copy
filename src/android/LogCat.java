7a3dc5eed266d9a97caff14e5b0a66e73b935517
vansnas <vanessanassali@gmail.com>
2024-10-09 09:32:38 +0100

    Update LogCat.java

--- src/android/LogCat.java
+++ src/android/LogCat.java
@@ -3,46 +3,49 @@ package org.apache.cordova.logcat;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.onesignal.OneSignal;
 
-import java.io.File;
-import java.io.IOException;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.os.Environment;
 import android.app.Activity;
 import android.util.Log;
 
-public class LogCat extends CordovaPlugin { //LogCatPlugin
+public class LogCat extends CordovaPlugin {
 
     private static final String TAG = "LogCatPlugin";
 
+    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("sendLogs")) {
-            if(!foregroundServiceRunning()) {
+            if (!foregroundServiceRunning()) {
                 Activity activity = cordova.getActivity();
                 Intent serviceIntent = new Intent(activity, MyForegroundService.class);
-                activity.getApplicationContext().startForegroundService(serviceIntent);
+                
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    // For Android O and above, use startForegroundService
+                    activity.getApplicationContext().startForegroundService(serviceIntent);
+                } else {
+                    activity.getApplicationContext().startService(serviceIntent);
+                }
             }
             return true;
         } else if (action.equals("registerDevice")) {
-                Activity activityCordova = cordova.getActivity();
-                OneSignal.initWithContext(activityCordova);
-                OneSignal.setAppId(args.getString(0));
-                return true;
+            Activity activityCordova = cordova.getActivity();
+            OneSignal.initWithContext(activityCordova);
+            OneSignal.setAppId(args.getString(0));
+            return true;
         } else {
             return false;
         }
-    }        //create a new Intent to send the logs
-             //Intent serviceIntent = new Intent(cordova.getActivity(), MyForegroundService.class);
-             //serviceIntent.setaction(""); //string of the action that want to execute
+    }
 
-    //Checks if the foreground service is running
+    // Checks if the foreground service is already running
     public boolean foregroundServiceRunning() {
         Context context = cordova.getActivity().getApplicationContext();
         ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
@@ -53,5 +56,4 @@ public class LogCat extends CordovaPlugin { //LogCatPlugin
         }
         return false;
     }
-
 }
