4a7d04222fadba4935e15ff4c97cfe7490ba2cc9
vansnas <vanessanassali@gmail.com>
2024-10-09 09:36:33 +0100

    Update MyBroadcastReceiver.java

--- src/android/MyBroadcastReceiver.java
+++ src/android/MyBroadcastReceiver.java
@@ -3,6 +3,7 @@ package org.apache.cordova.logcat;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
@@ -11,10 +12,26 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "onReceive: action=" + intent.getAction());
-        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) || intent.getAction().equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
-            Intent serviceIntent = new Intent(context, MyForegroundService.class);
-            context.startForegroundService(serviceIntent);
+        if (intent == null || intent.getAction() == null) {
+            Log.e(TAG, "onReceive: Null intent or action");
+            return;
+        }
+
+        String action = intent.getAction();
+        Log.i(TAG, "onReceive: action=" + action);
+
+        if (action.equals(Intent.ACTION_BOOT_COMPLETED) || action.equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                Intent serviceIntent = new Intent(context, MyForegroundService.class);
+                context.startForegroundService(serviceIntent);
+                Log.i(TAG, "Started foreground service: MyForegroundService");
+            } else {
+                Intent serviceIntent = new Intent(context, MyForegroundService.class);
+                context.startService(serviceIntent);
+                Log.i(TAG, "Started service: MyForegroundService");
+            }
+        } else {
+            Log.w(TAG, "Unhandled action: " + action);
         }
     }
 }
